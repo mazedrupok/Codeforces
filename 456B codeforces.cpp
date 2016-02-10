e0489f02a1e6244776612436d99cd80e6d0fe4f6
Md. Abdul Mazed Rupok <rupurup@gmail.com>
2016-02-10 19:19:32 +0600

    Create 456B codeforces.cpp

---
+++ 456B codeforces.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+int main ()
+{
+    int n;
+    scanf (" %d", &n);
+    //cout << n << endl;
+    if (n %4 == 0) printf ("4\n");
+    else printf ("0\n");
+    return 0;
+}
